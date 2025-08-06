a053259739a254d3862747dbfa505bb0e64179dc
wiarek <leszczyszynmarek08@gmail.com>
2025-08-06 08:54:36 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'thailand-2026-cache-v1';
+const urlsToCache = [
+  '/Thailand/',
+  '/Thailand/index.html',
+  '/Thailand/home.html',
+  '/Thailand/manifest.json',
+  '/Thailand/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
