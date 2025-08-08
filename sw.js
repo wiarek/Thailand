90ff76a47ad9ff6bc85a9af2429a234c8a5ec2d5
wiarek <leszczyszynmarek08@gmail.com>
2025-08-08 07:24:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'thailand-2026-cache-v3'; // UWAGA: zwiększ przy każdej aktualizacji!
+const CACHE_NAME = 'thailand-2026-cache-v4'; // UWAGA: zwiększ przy każdej aktualizacji!
 const urlsToCache = [
   '/Thailand/',
   '/Thailand/index.html',
